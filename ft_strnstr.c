cb53ee47bc4e3d53bc44b9f4033a884da3267b62
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-04-23 05:51:01 +0300

    updated

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,24 +14,19 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	h;
-	size_t	n;
+	size_t	needle_len;
+	size_t	i;
 
-	h = 0;
-	if (needle[0] == '\0')
+	i = 0;
+	needle_len = ft_strlen(needle);
+	if (!needle_len)
 		return ((char *)haystack);
-	while (haystack[h] != '\0')
+	while (haystack[i] && needle_len <= len)
 	{
-		n = 0;
-		while (haystack[h + n] == needle[n] && (h + n) < len)
-		{
-			if (haystack[h + n] == '\0' && needle[n] == '\0')
-				return ((char *)&haystack[h]);
-			n++;
-		}
-		if (needle[n] == '\0')
-			return ((char *)haystack + h);
-		h++;
+		if (ft_strncmp(&haystack[i], needle, needle_len) == 0)
+			return ((char *)&haystack[i]);
+		i++;
+		len--;
 	}
 	return (0);
 }
