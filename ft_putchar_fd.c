35c76d73b6e89eda107b3c9711b5a94d8d0cc85e
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-04-23 23:51:55 +0300

    updated

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -14,18 +14,16 @@
 
 void	ft_putchar_fd(char c, int fd)
 {
-	write (fd, &c, sizeof(char));
+	write(fd, &c, 1);
 }
-// Ekrana tek bir karakter yazar. Belirtilen dosyaya putchar gönderiyor.
+// file'a c parametresinin çıktısını yazar
+// O_CREAT => dosya oluşturur (mevcut değilse)
+// O_RDWR => dosyayı hem okuma hem de yazma erişimli açar
+// 0777 => bir dosya ya da klasöre okuma, yazma ve çalıştırma yetkisi verir.
 /*
-#include <stdio.h>
-int main () {
-   char ch;
-
-   for(ch = 'A' ; ch <= 'Z' ; ch++) {
-      putchar(ch);
-   }
-   
-   return(0);
+#include <fcntl.h>
+int main() {
+	int fd = open("a.txt", O_CREAT | O_RDWR, 0777);
+	ft_putchar_fd('b', fd);
 }
 */
