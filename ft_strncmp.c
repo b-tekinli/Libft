ce0a2391dfbd0b316a93087279c3e5dc86646546
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-04-23 07:03:29 +0300

    updated

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,15 +14,23 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n--)
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && n-- > 0)
 	{
-		if (*s1 != *s2)
-			return ((unsigned char)*s1 - (unsigned char)*s2);
-		else if (!*s1)
-			return (0);
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (((unsigned char) s1[i]) - ((unsigned char) s2[i]));
+			i++;
 	}
 	return (0);
 }
-// 2 stringi karşılaştırıp farkını geri döndürüyor.
+// 2 stringi karşılaştırıp char farkını geri döndürüyor.
+/*
+#include <stdio.h>
+int	main()
+{
+	char s1[] = "bt";
+	char s2[] = "bT";
+	printf("%d", ft_strncmp(s1, s2, 2));
+}*/
